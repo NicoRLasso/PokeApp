3c62a584e53aaa4b342de4c4d888eb65c891d45f
Nicolas Rodriguez Lasso <nicolas.rlasso@gmail.com>
2021-05-10 17:51:48 -0500

    add login with jwt

--- pokeapp/api/urls.py
+++ pokeapp/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import main,registerUser
+from .views import main,registerUser,MyTokenObtainPairView
 
 urlpatterns = [
     path('',main),
     path('register/',registerUser, name="register"),
+    path('login/',MyTokenObtainPairView.as_view(),name='token_obtain_pair'),
 ]
--- pokeapp/api/views.py
+++ pokeapp/api/views.py
@@ -1,11 +1,13 @@
 from .serializers import UserSerializer
 from django.shortcuts import render
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view,permission_classes
 from rest_framework.response import Response
 from django.contrib.auth.models import User
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 from django.contrib.auth.hashers import make_password
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     def validate(self,attrs):
@@ -29,12 +31,12 @@ def registerUser(request):
     try:
         user = User.objects.create(
             first_name=data['name'],
-            username=data['email'],
+            username=data['username'],
             email=data['email'],
             password=make_password(data['password'])
         )
         serializer = UserSerializer(user,many=False)
         return Response(serializer.data)
     except:
-        message = {'detail':'User with this email already exist'}
+        message = {'detail':'User with this Username already exist'}
         return Response(message,status=status.HTTP_400_BAD_REQUEST)
