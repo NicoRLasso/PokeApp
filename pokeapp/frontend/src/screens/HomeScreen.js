637f0fe5c29d0891da0da474a11dba04b1cc6e93
Nicolas Rodriguez Lasso <nicolas.rlasso@gmail.com>
2021-05-10 19:44:24 -0500

    finishing home screen panel

--- pokeapp/frontend/src/screens/HomeScreen.js
+++ pokeapp/frontend/src/screens/HomeScreen.js
@@ -1,27 +1,41 @@
 import React, { Component } from "react";
-import Loader from "../components/Loader";
-import {
-  Row,
-  Col,
-  ListGroup,
-  Image,
-  Form,
-  Button,
-  Card,
-} from "react-bootstrap";
+import { Row, Col, Image, Form } from "react-bootstrap";
 export default class HomeScreen extends Component {
+  constructor() {
+    super();
+    this.state = { data: [] };
+  }
+  async componentDidMount() {
+    const response = await fetch("https://pokeapi.co/api/v2/pokemon/");
+    const json = await response.json();
+    this.setState({ data: json });
+    this.buscarpokemon = this.buscarpokemon.bind(this);
+    this.renderpokemon = this.renderpokemon.bind(this);
+  }
+  buscarpokemon(id) {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+      .then((response) => response.json())
+      .then((data) => this.renderpokemon(data));
+  }
+  renderpokemon(data) {
+    console.log(data);
+  }
   render() {
+    const selectpokemon = (e) => {
+      let id = e.target.value;
+      this.buscarpokemon(id);
+    };
     return (
       <Row>
-        <Col md={6}>
+        <Col md={6} xs={12}>
           <h3>
             En esta pagina encontraras lo relacionado a los POKEMONS. Te
             invitamos a descubrir que mas caracteristicas pueden tener estos
-            fieles compañeros
+            fieles compañeros.
           </h3>
           <Row>
             <h4 className="text-info"> Checa estos Amigos de aca</h4>
-            <Col md={4}>
+            <Col md={4} xs={4}>
               <Image
                 src="../../static/images/poke1.png"
                 alt="poke1"
@@ -29,7 +43,7 @@ export default class HomeScreen extends Component {
                 rounded
               />
             </Col>
-            <Col md={4}>
+            <Col md={4} xs={4}>
               <Image
                 src="../../static/images/poke2.png"
                 alt="poke2"
@@ -37,7 +51,7 @@ export default class HomeScreen extends Component {
                 rounded
               />
             </Col>
-            <Col md={4}>
+            <Col md={4} xs={4}>
               <Image
                 src="../../static/images/poke3.png"
                 alt="poke3"
@@ -46,9 +60,28 @@ export default class HomeScreen extends Component {
               />
             </Col>
           </Row>
-          <h4 className="text-info"> Quieres Conocer mas hay </h4>
         </Col>
-        <Col md={6}></Col>
+        <Col md={6} xs={12}>
+          <Row>
+            <h3>
+              Quieres Conocer mas?
+              <br /> Hay mas de {this.state.data.count} pokemos por descrubrir
+              <Form.Control
+                as="select"
+                custom
+                id="escoPokemon"
+                onChange={selectpokemon}
+              >
+                {[...Array(this.state.data.count).keys()].map((x) => (
+                  <option value={x + 1} key={x + 1}>
+                    {x + 1}
+                  </option>
+                ))}
+              </Form.Control>
+            </h3>
+          </Row>
+          <Row id="cardforPokemon" hidden></Row>
+        </Col>
       </Row>
     );
   }
